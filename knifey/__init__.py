767a31d3db559bf41772c8216e8fae946e91823a
Gabriel Franco <gabefgonc@gmail.com>
2020-09-16 06:34:17 -0300

    added find method

--- knifey/__init__.py
+++ knifey/__init__.py
@@ -1,9 +1,17 @@
 import collections
+import re
+import subprocess
 
 class PreCommand():
     def __init__(self, command):
         self.command = command
+        self.mustSearch = False
+        self.searchTerm = ""
     
     def pipe(self, toPipe):
         self.command.append(" | ")
         self.command.append(toPipe)
+
+    def find(self, string):
+        self.mustSearch = True
+        self.searchTerm = string
